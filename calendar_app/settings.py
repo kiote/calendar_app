bbc246be3928fe559f954987c9b3e2c626079c2b
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-11 18:28:15 +0300

    more static files info added

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -88,6 +88,12 @@ else:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 
 # Internationalization
