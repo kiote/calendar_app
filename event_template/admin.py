176438f6dce708fcc9cf216c792a3b6dfafb8393
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-14 18:39:27 +0300

    Add readonly field

--- event_template/admin.py
+++ event_template/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import EventTemplate
 
+class EventTemplateAdmin(admin.ModelAdmin):
+    readonly_fields = ('event_data_json', )
+
 admin.site.register(EventTemplate)
