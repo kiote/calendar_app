51626e3dc4184eaa8639b8048d7b8bf3d06e03d6
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-14 18:42:29 +0300

    right way to register

--- event_template/admin.py
+++ event_template/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import EventTemplate
 
+@admin.register(EventTemplate)
 class EventTemplateAdmin(admin.ModelAdmin):
     readonly_fields = ('event_data_json', )
-
-admin.site.register(EventTemplate)
