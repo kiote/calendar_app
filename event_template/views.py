d78419347a6709e6cb260afad5dcfe74cdf11bf7
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-18 10:39:01 +0300

    add single event/link to single event

--- event_template/views.py
+++ event_template/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template import RequestContext
+from django.contrib import messages
+from django.shortcuts import redirect
 
 from .models import EventTemplate
 
 class ListView(TemplateView):
-    template_name = 'index.html'
-
     def get(self, request):
         events = EventTemplate.objects.all()
         context = RequestContext(request, {'events': events})
         return render(request, 'event_template/index.html', context)
+
+class SingleView(TemplateView):
+    def get(self, request, event_id):
+        messages.success(request, 'Event has been added to your calendar.')
+        return redirect('list')
