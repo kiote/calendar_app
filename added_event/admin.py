77a2e0cbc38cd3c26fefc49692b8ddd9b4b28f59
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-19 22:43:34 +0300

    new admin model

--- added_event/admin.py
+++ added_event/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AddedEvent
 
-# Register your models here.
+@admin.register(AddedEvent)
+class EventTemplateAdmin(admin.ModelAdmin):
+    readonly_fields = ('summary', 'time_start', 'time_end', 'created_at', 'guser', 'event')
