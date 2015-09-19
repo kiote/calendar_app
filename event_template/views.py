e46a51c00745752b9563f6129eba777e7084cff1
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-19 21:48:35 +0300

    add event to calendar

--- event_template/views.py
+++ event_template/views.py
@@ -1,3 +1,7 @@
+from oauth2client import client
+import httplib2
+from apiclient import discovery
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template import RequestContext
@@ -5,6 +9,8 @@ from django.contrib import messages
 from django.shortcuts import redirect
 
 from .models import EventTemplate
+from guser.models import Guser
+from added_event.models import AddedEvent
 
 class ListView(TemplateView):
     def get(self, request):
@@ -14,5 +20,25 @@ class ListView(TemplateView):
 
 class SingleView(TemplateView):
     def get(self, request, event_id):
+        """this action adds event to user's calendar"""
+        if 'credentials' not in request.session:
+            return redirect('callback')
+        credentials = client.OAuth2Credentials.from_json(request.session['credentials'])
+        if credentials.access_token_expired:
+            return redirect('callback')
+        http_auth = credentials.authorize(httplib2.Http())
+
+        # add or create user to database
+        user, created = Guser.objects.get_or_create(email=Guser.discover_email(http_auth))
+
+        # create user attempt to add event to database
+        event = EventTemplate.objects.get(id=event_id)
+        AddedEvent.objects.create(guser=user, event=event)
+
+        # call google api to create event
+        event_json = event.event_data_json
+        service = discovery.build('calendar', 'v3', http=http_auth)
+        service.events().insert(calendarId='primary',
+                                body=event_json).execute()
         messages.success(request, 'Event has been added to your calendar.')
         return redirect('list')
--- guser/models.py
+++ guser/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+from apiclient import discovery
+
 class Guser(models.Model):
     email = models.EmailField()
     created_at = models.DateTimeField(default=timezone.now)
+
+    @staticmethod
+    def discover_email(http_auth):
+        user_info_service = discovery.build(serviceName='oauth2',
+                                            version='v2',
+                                            http=http_auth)
+        user_info = user_info_service.userinfo().get().execute()
+        return user_info['email']
