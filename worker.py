394a5f19c0e04b53f4907bf5a6f26b782c124b04
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-28 16:05:10 +0300

    make worker to be able to run

--- worker.py
+++ worker.py
@@ -1,3 +1,8 @@
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calendar_app.settings")
+django.setup()
+
 from added_event.models import AddedEvent
 
 AddedEvent.objects.check_same()
