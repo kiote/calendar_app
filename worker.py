eb0d784ae6bcd2537063888e0bc8c5fbe5417279
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-16 16:36:35 +0300

    Cosmetic changes of the worker

--- worker.py
+++ worker.py
@@ -1,8 +1,8 @@
 import os
 import django
+from added_event.models import AddedEvent
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calendar_app.settings")
 django.setup()
-
-from added_event.models import AddedEvent
 
 AddedEvent.objects.check_same()
