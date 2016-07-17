5a34e3db768fdeee1b166dfe023998ee6224f7c0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-17 23:51:57 +0300

    Remove homeview

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from google_auth.views import HomeView
 from google_auth.views import AuthView
 from event_template.views import ListView
 from event_template.views import SingleView
@@ -10,5 +9,4 @@ urlpatterns = [
     url(r'^oauth2callback/', AuthView.as_view(), name='callback'),
     url(r'^events/', ListView.as_view(), name='list'),
     url(r'^event/([0-9]+)/$', SingleView.as_view(), name='single_event'),
-    url(r'^$', HomeView.as_view(), name='index')
 ]
--- google_auth/views.py
+++ google_auth/views.py
@@ -13,17 +13,6 @@ from calendar_app.settings import HOST_NAME
 SCOPES = ['https://www.googleapis.com/auth/calendar', 'email', 'profile']
 
 
-class HomeView(TemplateView):
-    def get(self, request):
-        if 'credentials' not in request.session:
-            return redirect('callback')
-        credentials = client.OAuth2Credentials.from_json(request.session['credentials'])
-        if credentials.access_token_expired:
-            return redirect('callback')
-        else:
-            return redirect('list')
-
-
 class AuthView(TemplateView):
     def get(self, request):
         event_id = request.session['event_id']
