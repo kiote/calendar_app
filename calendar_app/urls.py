dd568d42164acefac1f3fd7fa00ef088e5948471
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-18 18:40:49 +0300

    Redirect to event
      before we shown only a link to event, so user have to open created event manually. With this commit we'll redirect to created event immideatelly.

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from google_auth.views import AuthView
-from event_template.views import ListView
 from event_template.views import SingleView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth2callback/', AuthView.as_view(), name='callback'),
-    url(r'^events/', ListView.as_view(), name='list'),
     url(r'^event/([0-9]+)/$', SingleView.as_view(), name='single_event'),
 ]
--- event_template/views.py
+++ event_template/views.py
@@ -1,40 +1,72 @@
 import httplib2
 import json
 
+from functools import wraps
+
 from oauth2client import client
 from apiclient import discovery
 
-from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.template import RequestContext
-from django.contrib import messages
 from django.shortcuts import redirect
 
-from .models import EventTemplate
+from event_template.models import EventTemplate
 from guser.models import Guser
 from added_event.models import AddedEvent
 
 
-class ListView(TemplateView):
-    def get(self, request):
-        events = EventTemplate.objects.all()
-        context = RequestContext(request, {'events': events})
-        return render(request, 'event_template/index.html', context)
+def _have_no_credentials(request):
+    """Check if session has no credentials"""
+    return 'credentials' not in request.session
+
+
+def _get_credentials(request):
+    return client.OAuth2Credentials.from_json(request.session['credentials'])
+
+
+def set_session_event_id(function):
+    """
+    Sets eventId to session.
+
+    We need this to be able to reirect back after login through google.
+    """
+    @wraps(function)
+    def set_event_id(self, request, event_id):
+        request.session['event_id'] = event_id
+        return function(self, request, event_id)
+
+    return set_event_id
+
+
+def must_have_google_credentials(function):
+    """
+    Checks Google credentials.
+
+    If we don't have them, or have expired, redirect to auth page
+    """
+    @wraps(function)
+    def check_credentials(self, request, event_id):
+        if _have_no_credentials(request):
+            return redirect('callback')
+
+        credentials = _get_credentials(request)
+
+        if credentials.access_token_expired:
+            return redirect('callback')
+
+        return function(self, request, event_id)
+
+    return check_credentials
 
 
 class SingleView(TemplateView):
+    @set_session_event_id
+    @must_have_google_credentials
     def get(self, request, event_id):
         """this action adds event to user's calendar"""
-        request.session['event_id'] = event_id
-
-        if 'credentials' not in request.session:
-            return redirect('callback')
-        credentials = client.OAuth2Credentials.from_json(request.session['credentials'])
-        if credentials.access_token_expired:
-            return redirect('callback')
+        credentials = _get_credentials(request)
         http_auth = credentials.authorize(httplib2.Http())
 
-        # add or create user to database
+        # add or create user into database
         email = Guser.discover_email(http_auth)
         user, created = Guser.objects.get_or_create(email=email)
 
@@ -44,7 +76,7 @@ class SingleView(TemplateView):
         event_json = json.loads(event.event_data_json)
         service = discovery.build('calendar', 'v3', http=http_auth)
         created_event = service.events().insert(calendarId='primary',
-                                body=event_json).execute()
+                                                body=event_json).execute()
 
         # create user attempt to add event to database
         AddedEvent.objects.create(guser=user,
@@ -52,5 +84,4 @@ class SingleView(TemplateView):
                                   credentials=request.session['credentials'],
                                   google_event_id=created_event.get('id'))
 
-        messages.success(request, 'Event has been added to your calendar. ' + created_event.get('htmlLink'))
-        return redirect('list')
+        return redirect(created_event.get('htmlLink'))
