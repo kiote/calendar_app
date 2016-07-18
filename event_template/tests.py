f2b502dbdc3d36e5ac7ea3aa4bd6b2a65fdedc5f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-18 14:18:25 +0300

    Add redirect testing in view

--- event_template/tests.py
+++ event_template/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
 from .models import EventTemplate
 import json
+from django.test import Client
+from django.core.urlresolvers import reverse
+
+
+def created_event_template(summary='Hi'):
+    return EventTemplate.objects.create(summary=summary)
+
 
 class EventTemplateTestCase(TestCase):
     def setUp(self):
-        self.subject = EventTemplate.objects.create(summary='Hi')
+        self.subject = created_event_template()
 
     def test_to_json(self):
         json_string = self.subject.to_json()
@@ -12,3 +19,14 @@ class EventTemplateTestCase(TestCase):
 
     def test_event_data_json(self):
         self.assertTrue("Hi" in self.subject.event_data_json)
+
+
+class SingleViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.event_template = created_event_template()
+
+    def test_get_with_no_credentials(self):
+        event_id = self.event_template.id
+        response = self.client.get(reverse('single_event', args=(event_id,)))
+        self.assertEquals(response.status_code, 302)
